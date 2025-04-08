f1fc992f4ec43fd7fbd343aa66130756ee658395
6BirdAdmin <login@6bird.studio>
2025-04-07 22:41:26 -0400

    Implemented Chain example from Rigify documentation

---
+++ __init__.py
@@ -0,0 +1,8 @@
+rigify_info = {
+    "name": "6 Bird Studio Rigify Expansion",
+    "author": "Jump",
+    "description":
+        "A series of rig additions as a feature set of the Rigify Extension for Blender.",
+    "link": "https://6bird.studio",
+    "blender": (4, 4, 0),
+}
