e37ed28c6f5fbd83d8d2a8dc92d977479984bb01
6BirdAdmin <login@6bird.studio>
2025-04-12 04:53:45 -0400

    slider pad initial implementation

--- rigs/slider_pad.py
+++ rigs/slider_pad.py
@@ -17,6 +17,7 @@
 # ======================= END GPL LICENSE BLOCK ========================
 
 import bpy
+import math
 from itertools import count
 from mathutils import Matrix, Vector
 from rigify.base_rig import stage, BaseRig
@@ -24,7 +25,7 @@ from rigify.utils.rig import connected_children_names
 from rigify.utils.misc import map_list
 from rigify.utils.bones import put_bone, copy_bone_properties, align_bone_orientation, set_bone_widget_transform
 from rigify.utils.naming import make_derived_name
-from rigify.utils.widgets_basic import create_bone_widget, create_line_widget
+from rigify.utils.widgets_basic import create_circle_widget, create_cube_widget
 from rigify.utils.widgets import adjust_widget_transform_mesh
 
 class Rig(BaseRig):
@@ -59,8 +60,8 @@ class Rig(BaseRig):
             self.configure_control_bone(*args)
         self.make_constraint(self.bones.ctrl[1], 'LIMIT_LOCATION', space_object= self.obj, \
             space_subtarget= self.bones.ctrl[0], owner_space ='LOCAL', use_transform_limit=True, \
-                max_y=bone1_length, use_max_x=True, use_max_y=True, \
-                    use_max_z=True, use_min_x=True, use_min_y=True, use_min_z=True)
+                max_x=(bone1_length), max_y=(bone1_length), min_x=(-1 * bone1_length), min_y=(-1 * bone1_length),\
+                    use_max_x=True, use_max_y=True, use_max_z=True, use_min_x=True, use_min_y=True, use_min_z=True)
 
     def configure_control_bone(self, i, ctrl, org):
         self.copy_bone_properties(org, ctrl)
@@ -74,7 +75,9 @@ class Rig(BaseRig):
         #Not gonna lie I got AI to figure this out, idk how it works
         bone1_name = self.bones.ctrl[0]
         bone2_name = self.bones.ctrl[1]
-        custom_prop_name = "bone_distance"
+        custom_prop_name_x = "bone_distance_x"
+        #custom_prop_name_y = "bone_distance_y"
+        custom_prop_name_z = "bone_distance_z"
 
         # Get objects and pose bones
         arm = self.obj
@@ -83,17 +86,28 @@ class Rig(BaseRig):
         bone1_length = pb1.bone.length
 
         # Add the custom property to bone_1
-        pb1[custom_prop_name] = 0.0
+        pb1[custom_prop_name_x] = 0.0
+        #pb1[custom_prop_name_y] = 0.0
+        pb1[custom_prop_name_z] = 0.0
 
         # Create driver for the custom property
-        prop_path = f'pose.bones["{bone1_name}"]["{custom_prop_name}"]'
-        fcurve = arm.driver_add(prop_path)
-        driver = fcurve.driver
-        driver.type = 'SCRIPTED'
+        prop_path_x = f'pose.bones["{bone1_name}"]["{custom_prop_name_x}"]'
+        #prop_path_y = f'pose.bones["{bone1_name}"]["{custom_prop_name_y}"]'
+        prop_path_z = f'pose.bones["{bone1_name}"]["{custom_prop_name_z}"]'
+        fcurve_x = arm.driver_add(prop_path_x)
+        #fcurve_y = arm.driver_add(prop_path_y)
+        fcurve_z = arm.driver_add(prop_path_z)
+        driver_x = fcurve_x.driver
+        #driver_y = fcurve_y.driver
+        driver_z = fcurve_z.driver
+        driver_x.type = 'SCRIPTED'
+        #driver_y.type = 'SCRIPTED'
+        driver_z.type = 'SCRIPTED'
+
 
         # Add variables for bone_1 and bone_2 world locations
         for bone, prefix in [(bone1_name, "a"), (bone2_name, "b")]:
-            for axis in "XYZ":
+            for axis, driver in zip("XZ", [driver_x, driver_z]):
                 var = driver.variables.new()
                 var.name = f"{prefix}_{axis}"
                 var.type = 'TRANSFORMS'
@@ -103,8 +117,14 @@ class Rig(BaseRig):
                 target.transform_type = f"LOC_{axis}"
                 target.transform_space = 'WORLD_SPACE'
 
-        driver.expression = (
-            f"min(sqrt((a_X - b_X)**2 + (a_Y - b_Y)**2 + (a_Z - b_Z)**2) / {bone1_length:.6f}, 1.0)"
+        driver_x.expression = (
+            f"min((b_X - a_X) / {bone1_length:.6f}, 1.0)"
+        )
+        # driver_y.expression = (
+        #     f"min((a_Y - b_Y) / {bone1_length:.6f}, 1.0)"
+        # )
+        driver_z.expression = (
+            f"min((b_Z - a_Z) / {bone1_length:.6f}, 1.0)"
         )
     
     def lock_bones(self):
@@ -124,8 +144,10 @@ class Rig(BaseRig):
     @stage.generate_widgets
     def make_control_widgets(self):
         ctrl = self.bones.ctrl
-        line = create_line_widget(self.obj, ctrl[0])
-        slider = create_bone_widget(self.obj, ctrl[1])
-        transform = Matrix.Scale(0.025, 4, Vector((0.0, 1.0, 0.0)))
-        adjust_widget_transform_mesh(slider, transform, local=True)
+        box = create_cube_widget(self.obj, ctrl[0])
+        slider = create_circle_widget(self.obj, ctrl[1])
+        transform_box = Matrix.Scale(0.001, 4, Vector((0, 0, 1))) @ Matrix.Scale(2.2, 4, Vector((0, 1, 0))) @ Matrix.Scale(2.2, 4, Vector((1, 0, 0)))
+        transform_slider =  Matrix.Rotation(math.radians(90), 4, 'X') @ Matrix.Scale(0.1, 4, Vector((1, 0, 0))) @ Matrix.Scale(0.1, 4, Vector((0, 0, 1)))
+        adjust_widget_transform_mesh(box, transform_box, local=True)
+        adjust_widget_transform_mesh(slider, transform_slider, local=True)
         
